aa40a2d32a0ab460e5dc91848e49aae32c99f77d
Jacob Sipple <sipple3@gmail.com>
2019-01-15 18:48:07 -0500

    fixed bug with getting the gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,14 @@
 let api = "https://api.giphy.com/v1/gifs/search?";
 let apiKey = "&api_key=xtum0fzlz72y6jqpyoxm1zQuqg3Hg1AC";
 // this will be what im seaching url will go api + q + apiKey
-let q = "&q=bob";
+let q = "&q=star+wars";
 let url;
 function getURL() {
     url = api + apiKey + q
-    $.getJSON(url, function(gif) {
-        createImg(gif.data[0].images.fixed_height.url)
+    $.getJSON(url, function(obj) {
+        for (let i = 0; i < obj.data.length; i++)
+        // change this to create an image tag not look for one in the html
+        $("#img").attr("src", obj.data[i].images.fixed_height_still.url)
     })
 }
 getURL()
