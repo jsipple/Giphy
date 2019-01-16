1346f25cab04c9fc8379b43c5dd005d421e38aa1
Jacob Sipple <sipple3@gmail.com>
2019-01-15 21:30:11 -0500

    added form where buttons are added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,18 @@ let apiKey = "&api_key=xtum0fzlz72y6jqpyoxm1zQuqg3Hg1AC";
 // this will be what im seaching url will go api + q + apiKey
 let q = "&q=star+wars";
 let url;
+// set variable for if running to get a different one either still or active
 function getURL() {
     url = api + apiKey + q
-    $.getJSON(url, function(gif) {
+    $.get(url).then(function(gif) {
         for (let i = 0; i < gif.data.length; i++)
         // below creates an image tag and adds it currently showing 25 gifs
-        $("#gifs").append($("<img>").attr("src", gif.data[i].images.original.url));
+        $("#gifs").append($("<img>").attr("src", gif.data[i].images.original_still.url));
     })
 }
+// below creating a button but not adding text
+$("#submit").on("click", function() {
+    $("#buttons").append($("<button>").text($("#submit").val()))
+})
 getURL()
 console.log(url)
